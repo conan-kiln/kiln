ffff8bb9d65dab32930a9a6c3402515a2814152b
Uilian Ries <uilianr@jfrog.com>
2025-08-28 16:27:14 +0300

    gdal: fix issue with libdeflate (#28281)
    * Do not patch libdeflate
    
    ---------
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    (cherry picked from commit e78938b75ce6ba93c9a7ec5f4a38e0ed7b3c27f0)

--- recipes/gdal/post_3.5.0/conanfile.py
+++ recipes/gdal/post_3.5.0/conanfile.py
@@ -625,10 +625,6 @@ class GdalConan(ConanFile):
         deps.generate()
 
     def _patch_sources(self):
-        # Fix Deflate::Deflate not being correctly propagated internally.
-        replace_in_file(self, os.path.join(self.source_folder, "port", "CMakeLists.txt"),
-                        "PRIVATE Deflate::Deflate",
-                        "PUBLIC Deflate::Deflate")
         # Workaround for JXL_THREADS being provided by the JXL package on CCI.
         replace_in_file(self, os.path.join(self.source_folder, "cmake", "helpers", "CheckDependentLibraries.cmake"),
                         "JXL_THREADS", "JXL", strict=False)
