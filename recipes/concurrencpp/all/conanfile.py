921763b09878df066f429dbf0e54817cbe73278d
PerseoGI <perseog@jfrog.com>
2025-08-29 10:45:45 +0300

    concurrencpp: add missing includes, maintenance changes (#28285)
    * fix: 🐛 concurrencpp: add missing includes
    
    * Relax libc++ requirement when using clang
    
    * Restrict clang > 14 and gcc > 13
    
    (cherry picked from commit 489617082a68e26cd8c82bfb411c44de71fdebd3)

--- recipes/concurrencpp/all/conanfile.py
+++ recipes/concurrencpp/all/conanfile.py
@@ -5,7 +5,6 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import *
-from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
@@ -14,10 +13,9 @@ required_conan_version = ">=2.1"
 class ConcurrencppConan(ConanFile):
     name = "concurrencpp"
     description = "Modern concurrency for C++. Tasks, executors, timers and C++20 coroutines to rule them all."
+    license = "MIT"
     homepage = "https://github.com/David-Haim/concurrencpp"
     topics = ("scheduler", "coroutines", "concurrency", "tasks", "executors", "timers", "await", "multithreading")
-    license = "MIT"
-    url = "https://github.com/conan-io/conan-center-index"
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -31,15 +29,10 @@ class ConcurrencppConan(ConanFile):
     implements = ["auto_shared_fpic"]
 
     @property
-    def _min_cppstd(self):
-        return "20"
-
-    @property
     def _minimum_compilers_version(self):
         return {
-            "msvc": "192",
-            "clang": "11",
-            "gcc": "13",
+            "clang": "14", # Until clang-14 coroutines are not fully supported
+            "gcc": "13",   # Until gcc-13 alignas specifier is not supported
         }
 
     def export_sources(self):
@@ -49,23 +42,10 @@ class ConcurrencppConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
-        check_min_cppstd(self, self._min_cppstd)
-        if Version(self.version) <= "0.1.5" and self.options.shared and is_msvc(self):
-            # see https://github.com/David-Haim/concurrencpp/issues/75
-            raise ConanInvalidConfiguration(f"{self.ref} does not support shared builds with Visual Studio")
-        if Version(self.version) <= "0.1.6" and self.settings.compiler == "gcc":
-            raise ConanInvalidConfiguration("gcc is not supported by concurrencpp")
-        if Version(self.version) >= "0.1.5" and self.settings.compiler == "apple-clang":
-            # apple-clang does not seem to support the C++20 synchronization library which concurrencpp 0.1.5 depends on
-            raise ConanInvalidConfiguration("apple-clang is not supported by concurrencpp 0.1.5 and higher")
-
+        check_min_cppstd(self, 20)
         minimum_version = self._minimum_compilers_version.get(str(self.settings.compiler), False)
         if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
-        if self.settings.compiler == "clang" and self.settings.compiler.libcxx != "libc++":
-            raise ConanInvalidConfiguration("libc++ required")
+            raise ConanInvalidConfiguration(f"{self.ref} requires C++20, which your compiler does not support.")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
