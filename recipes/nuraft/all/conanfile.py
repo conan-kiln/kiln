e19b704b5af33dfae41c16969d20e288a1da1395
Andi Skrgat <andi8647@gmail.com>
2025-09-02 10:58:44 +0300

    nuraft: add version 3.0.0 (#27919)
    * Upgrade Conanfile and add 3.0.0
    
    * Remove changes nuraft
    
    * Remove import
    
    * Revert option for asio
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    * Do not build examples by default
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    * Add zlib as requirement
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    * Update cpp_info for correct cmake targets!
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    * Remove extra libraries
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    * Respect change for verison 3.x
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    * Zlib is not actually used
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    * Adapt test package to work for both majors
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    * Version 3.x needs cmake 3.26
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    * Fix bad logic for cmake as build requirement
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    * Build mac as shared for version 3
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    ---------
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    Co-authored-by: Uilian Ries <uilianr@jfrog.com>
    
    (cherry picked from commit 026ef641861a4657d89f66c4fa8cfa19c59f3d71)

--- recipes/nuraft/all/conanfile.py
+++ recipes/nuraft/all/conanfile.py
@@ -5,18 +5,17 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import *
+from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
 
 class NuRaftConan(ConanFile):
     name = "nuraft"
+    description = "Cornerstone based RAFT library."
     homepage = "https://github.com/eBay/NuRaft"
-    description = """Cornerstone based RAFT library."""
     topics = ("raft",)
-    url = "https://github.com/conan-io/conan-center-index"
     license = "Apache-2.0"
-
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -37,17 +36,24 @@ class NuRaftConan(ConanFile):
     def layout(self):
         cmake_layout(self, src_folder="src")
 
+    def build_requirements(self):
+        if Version(self.version) >= 3:
+            self.tool_requires("cmake/[>=3.26]")
+
     def requirements(self):
         self.requires("openssl/[>=1.1 <4]")
         if self.options.asio == "boost":
-            self.requires("boost/[^1.71.0 <1.88]", libs=False)
+            if Version(self.version) >= 3:
+                self.requires("boost/[^1.71.0]", libs=False)
+            else:
+                self.requires("boost/[^1.71.0 <1.88]", libs=False)
         else:
             self.requires("asio/[^1.27.0]", libs=False)
 
     def validate(self):
         if self.settings.os == "Windows":
             raise ConanInvalidConfiguration(f"{self.ref} doesn't support Windows")
-        if self.settings.os == "Macos" and self.options.shared:
+        if Version(self.version) < 3 and self.settings.os == "Macos" and self.options.shared:
             raise ConanInvalidConfiguration(f"{self.ref} shared not supported for Macos")
         check_min_cppstd(self, 11)
 
@@ -57,7 +63,11 @@ class NuRaftConan(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
-        if self.options.asio != "boost":
+        if Version(self.version) >= 3:
+            tc.cache_variables["WITH_CONAN"] = True
+            tc.cache_variables["BOOST_ASIO"] = self.options.asio == "boost"
+            tc.cache_variables["BUILD_EXAMPLES"] = False
+        elif self.options.asio != "boost":
             tc.cache_variables["CMAKE_DISABLE_FIND_PACKAGE_Boost"] = True
         tc.generate()
         deps = CMakeDeps(self)
@@ -72,8 +82,23 @@ class NuRaftConan(ConanFile):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        if self.options.shared:
+            rm(self, "*.a", os.path.join(self.package_folder, "lib"))
+            rm(self, "*.lib", os.path.join(self.package_folder, "lib"))
+        else:
+            rm(self, "*.so", os.path.join(self.package_folder, "lib"))
+            rm(self, "*.dylib", os.path.join(self.package_folder, "lib"))
+            rm(self, "*.dll", os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
+        if Version(self.version) >= 3:
+            self.cpp_info.set_property("cmake_file_name", "NuRaft")
+            if self.options.shared:
+                self.cpp_info.set_property("cmake_target_name", "NuRaft::shared_lib")
+            else:
+                self.cpp_info.set_property("cmake_target_name", "NuRaft::static_lib")
+
         self.cpp_info.libs = ["nuraft"]
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs.extend(["m", "pthread"])
