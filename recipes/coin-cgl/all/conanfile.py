48df3f38108575e6867c8040279c3c835b0fd78d
Martin Valgur <martin.valgur@gmail.com>
2025-09-09 12:00:42 +0300

    coin-cgl: add all optional deps

--- recipes/coin-cgl/all/conanfile.py
+++ recipes/coin-cgl/all/conanfile.py
@@ -21,12 +21,14 @@ class CoinCglConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "with_glpk": [True, False],
+        "with_dylp": [True, False],
+        "with_vol": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "with_glpk": True,
+        "with_dylp": True,
+        "with_vol": True,
     }
     implements = ["auto_shared_fpic"]
 
@@ -34,12 +36,12 @@ class CoinCglConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("coin-utils/[^2.11.11]")
         self.requires("coin-osi/[>=0.108.10 <1]")
         self.requires("coin-clp/[^1.17.9]")
-        if self.options.with_glpk:
-            self.requires("glpk/[<=4.48]")
-        # TODO: add support for: Cplex, Mosek, Xpress, Vol, DyLP
+        if self.options.with_dylp:
+            self.requires("coin-dylp/[^1.10.4]")
+        if self.options.with_vol:
+            self.requires("coin-vol/[^1.5.4]")
 
     def build_requirements(self):
         self.tool_requires("coin-buildtools/[*]")
@@ -55,15 +57,19 @@ class CoinCglConan(ConanFile):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
+        yes_no = lambda v: "yes" if v else "no"
+        osi = self.dependencies["coin-osi"]
         tc = AutotoolsToolchain(self)
         tc.configure_args.extend([
-            "--with-osiglpk" if self.options.with_glpk else "--without-osiglpk",
-            "--without-vol",
-            "--without-dylp",
-            "--without-osicpx",
-            "--without-osimsk",
-            "--without-osixpr",
-            "--without-osivol",
+            "--with-clp=yes",
+            "--with-osiclp=yes",
+            f"--with-osidylp={yes_no(self.options.with_dylp)}",
+            f"--with-osivol={yes_no(self.options.with_vol)}"
+            f"--with-osicplex={yes_no(osi.options.with_cplex)}"
+            f"--with-osiglpk={yes_no(osi.options.with_glpk)}"
+            f"--with-osigurobi={yes_no(osi.options.with_gurobi)}"
+            f"--with-osimosek={yes_no(osi.options.with_mosek)}"
+            f"--with-osixpress={yes_no(osi.options.with_xpress)}"
             "--without-sample",
             "--disable-dependency-linking",
             "F77=unavailable",
@@ -84,7 +90,6 @@ class CoinCglConan(ConanFile):
         tc.generate(env)
 
         deps = PkgConfigDeps(self)
-        deps.set_property("glpk", "pkg_config_aliases", ["coinglpk"])
         deps.generate()
 
     def build(self):
@@ -116,4 +121,4 @@ class CoinCglConan(ConanFile):
     def package_info(self):
         self.cpp_info.set_property("pkg_config_name", "cgl")
         self.cpp_info.libs = ["Cgl"]
-        self.cpp_info.includedirs.append(os.path.join("include", "coin"))
+        self.cpp_info.includedirs.append("include/coin")
--- recipes/coin-cgl/all/test_package/test_package.cpp
+++ recipes/coin-cgl/all/test_package/test_package.cpp
@@ -1,9 +1,8 @@
-#include "CglProbing.hpp"
+#include <CglProbing.hpp>
 
-int main(int argc, char *argv[])
+int main()
 {
     CglProbing probing;
     probing.setLogLevel(0);
     probing.setUsingObjective(1);
-    return probing.getUsingObjective() == 1 ? 0 : 1;
 }
