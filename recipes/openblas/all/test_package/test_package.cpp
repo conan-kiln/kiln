cfcf40c86db0fcaa8065297fab20a374aa77c00b
Martin Valgur <martin.valgur@gmail.com>
2025-09-14 09:40:53 +0300

    openblas: test both the C and Fortran APIs

--- recipes/openblas/all/test_package/test_package.cpp
+++ recipes/openblas/all/test_package/test_package.cpp
@@ -1,15 +1,41 @@
 #include <cblas.h>
+#include <f77blas.h>
 #include <stdio.h>
 
-int main()
-{
-  int i=0;
+void test_cblas() {
   double A[6] = {1.0,2.0,1.0,-3.0,4.0,-1.0};
   double B[6] = {1.0,2.0,1.0,-3.0,4.0,-1.0};
   double C[9] = {.5,.5,.5,.5,.5,.5,.5,.5,.5};
   cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans,3,3,2,1,A, 3, B, 3,2,C,3);
 
-  for(i=0; i<9; i++)
+  for(int i=0; i<9; i++)
     printf("%lf ", C[i]);
   printf("\n");
+}
+
+void test_f77blas() {
+  double A[6] = {1.0,2.0,1.0,-3.0,4.0,-1.0};
+  double B[6] = {1.0,2.0,1.0,-3.0,4.0,-1.0};
+  double C[9] = {.5,.5,.5,.5,.5,.5,.5,.5,.5};
+  char transa = 'N';
+  char transb = 'T';
+  blasint m = 3;
+  blasint n = 3;
+  blasint k = 2;
+  blasint lda = 3;
+  blasint ldb = 3;
+  blasint ldc = 3;
+  double alpha = 1.0;
+  double beta  = 2.0;
+  dgemm_(&transa, &transb, &m, &n, &k, &alpha, A, &lda, B, &ldb, &beta, C, &ldc);
+
+  for(int i=0; i<9; i++)
+    printf("%lf ", C[i]);
+  printf("\n");
+}
+
+int main()
+{
+  test_cblas();
+  test_f77blas();
 }
