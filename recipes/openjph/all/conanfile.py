c96663c4bfa128a3d9180c0841320a08f052d7f8
Ingmar Rieger <git@irieger.net>
2025-09-17 11:58:16 +0300

    openjph: Bump to version 0.23.1 (#28396)
    (cherry picked from commit 901e3b74b34e853e12210de0ff3d855f07082bab)

--- recipes/openjph/all/conanfile.py
+++ recipes/openjph/all/conanfile.py
@@ -50,7 +50,7 @@ class OpenJPH(ConanFile):
             self.requires("libtiff/[>=4.5 <5]")
 
     def validate(self):
-        check_min_cppstd(self, 11)
+        check_min_cppstd(self, 14)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
