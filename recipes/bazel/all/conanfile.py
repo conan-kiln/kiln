e1c34299d0154ef24f02e1cda8535006ea4f9e47
Martin Valgur <martin.valgur@gmail.com>
2025-09-19 12:24:51 +0300

    bazel: update all major versions

--- recipes/bazel/all/conanfile.py
+++ recipes/bazel/all/conanfile.py
@@ -3,21 +3,22 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.files import *
+from conan.tools.layout import basic_layout
 
 required_conan_version = ">=2.1"
 
 
 class BazelConan(ConanFile):
     name = "bazel"
-    package_type = "application"
     description = "Bazel is a fast, scalable, multi-language and extensible build system."
     license = "Apache-2.0"
     homepage = "https://bazel.build/"
     topics = ("test", "build", "automation", "pre-built")
+    package_type = "application"
     settings = "os", "arch", "compiler", "build_type"
 
     def layout(self):
-        pass
+        basic_layout(self, src_folder="src")
 
     def package_id(self):
         del self.info.settings.compiler
@@ -37,17 +38,6 @@ class BazelConan(ConanFile):
                 f"{self.settings.arch} architecture on {self.settings.os} is not supported for this package."
             )
 
-    def source(self):
-        pass
-
-    @property
-    def _bazel_filename(self):
-        return self._binary_info["url"].rsplit("/")[-1]
-
-    def build(self):
-        download(self, **self._binary_info, filename=self._bazel_filename)
-        download(self, **self.conan_data["sources"][self.version]["license"], filename="LICENSE")
-
     @property
     def _program_suffix(self):
         return ".exe" if self.settings.os == "Windows" else ""
@@ -57,22 +47,11 @@ class BazelConan(ConanFile):
         os.chmod(name, os.stat(name).st_mode | 0o111)
 
     def package(self):
-        copy(
-            self,
-            pattern="LICENSE",
-            dst=os.path.join(self.package_folder, "licenses"),
-            src=self.source_folder,
-        )
-        copy(
-            self,
-            pattern=self._bazel_filename,
-            dst=os.path.join(self.package_folder, "bin"),
-            src=self.source_folder,
-        )
-        old_target_filename = os.path.join(self.package_folder, "bin", self._bazel_filename)
-        new_target_filename = os.path.join(self.package_folder, "bin", "bazel" + self._program_suffix)
-        rename(self, old_target_filename, new_target_filename)
-        self._chmod_plus_x(new_target_filename)
+        download(self, **self.conan_data["sources"][self.version]["license"],
+                 filename=os.path.join(self.package_folder, "licenses", "LICENSE"))
+        bazel_exe = os.path.join(self.package_folder, "bin", "bazel" + self._program_suffix)
+        download(self, **self._binary_info, filename=bazel_exe)
+        self._chmod_plus_x(bazel_exe)
 
     def package_info(self):
         self.cpp_info.libdirs = []
