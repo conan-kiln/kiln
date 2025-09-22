5692b62c9622c65973a6083d437762ac5236e14a
Martin Valgur <martin.valgur@gmail.com>
2025-09-22 17:18:16 +0300

    krb5: fix recipe structure

--- recipes/krb5/all/conanfile.py
+++ recipes/krb5/all/conanfile.py
@@ -37,6 +37,7 @@ class Krb5Conan(ConanFile):
         "use_dns_realms": "Enable DNS for realms",
         "with_tls": "Enable TLS support with OpenSSL",
     }
+    implements = ["auto_shared_fpic"]
     languages = ["C"]
 
     def export_sources(self):
@@ -45,9 +46,20 @@ class Krb5Conan(ConanFile):
     def layout(self):
         basic_layout(self, src_folder="src")
 
+    def requirements(self):
+        self.requires("libverto/[>=0.3.2 <1]")
+        if self.options.with_tls == "openssl":
+            self.requires("openssl/[>=1.1 <4]")
+        if self.options.get_safe("with_tcl"):
+            self.requires("tcl/[^8.6.16]")
+
     def build_requirements(self):
         if self.options.i18n:
             self.tool_requires("gettext/[>=0.21 <1]", options={"tools": True})
+        if not self.conf.get("tools.gnu:pkg_config", check_type=str):
+            self.tool_requires("pkgconf/[>=2.2 <3]")
+        self.tool_requires("automake/[^1.18.1]")
+        self.tool_requires("bison/[^3.8.2]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -90,19 +102,6 @@ class Krb5Conan(ConanFile):
         pkg.generate()
         pkg = PkgConfigDeps(self)
         pkg.generate()
-
-    def requirements(self):
-        self.requires("libverto/0.3.2")
-        if self.options.with_tls == "openssl":
-            self.requires("openssl/[>=1.1 <4]")
-        if self.options.get_safe("with_tcl"):
-            self.requires("tcl/[^8.6.16]")
-
-    def build_requirements(self):
-        if not self.conf.get("tools.gnu:pkg_config", check_type=str):
-            self.tool_requires("pkgconf/[>=2.2 <3]")
-        self.tool_requires("automake/[^1.18.1]")
-        self.tool_requires("bison/[^3.8.2]")
 
     def build(self):
         with chdir(self, os.path.join(self.source_folder, "src")):
