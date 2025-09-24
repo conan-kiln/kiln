91c7702b4687b81897d94aac0642607307b888cf
Martin Valgur <martin.valgur@gmail.com>
2025-09-24 22:44:32 +0300

    taskflow: drop old versions

--- recipes/taskflow/all/conanfile.py
+++ recipes/taskflow/all/conanfile.py
@@ -1,12 +1,10 @@
 import os
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import *
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc
-from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
@@ -23,22 +21,6 @@ class TaskflowConan(ConanFile):
     package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
 
-    @property
-    def _min_cppstd(self):
-        return "17"
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "msvc": "192",
-            "gcc": "7.3" if Version(self.version) < "3.7.0" else "8.4",
-            "clang": "6.0",
-            "apple-clang": "10.0",
-        }
-
-    def export_sources(self):
-        export_conandata_patches(self)
-
     def layout(self):
         basic_layout(self, src_folder="src")
 
@@ -46,23 +28,10 @@ class TaskflowConan(ConanFile):
         self.info.clear()
 
     def validate(self):
-        check_min_cppstd(self, self._min_cppstd)
-
-        def loose_lt_semver(v1, v2):
-            lv1 = [int(v) for v in v1.split(".")]
-            lv2 = [int(v) for v in v2.split(".")]
-            min_length = min(len(lv1), len(lv2))
-            return lv1[:min_length] < lv2[:min_length]
-
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.",
-            )
+        check_min_cppstd(self, 17)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        apply_conandata_patches(self)
 
     def package(self):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
@@ -71,12 +40,13 @@ class TaskflowConan(ConanFile):
                   dst=os.path.join(self.package_folder, "include", "taskflow"))
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "Taskflow")
+        self.cpp_info.set_property("cmake_target_name", "Taskflow::Taskflow")
+
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
 
-        self.cpp_info.set_property("cmake_file_name", "Taskflow")
-        self.cpp_info.set_property("cmake_target_name", "Taskflow::Taskflow")
         if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.system_libs.append("pthread")
+            self.cpp_info.system_libs = ["pthread"]
         if is_msvc(self):
-            self.cpp_info.defines.append("_ENABLE_EXTENDED_ALIGNED_STORAGE")
+            self.cpp_info.defines = ["_ENABLE_EXTENDED_ALIGNED_STORAGE"]
