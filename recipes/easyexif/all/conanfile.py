be0cca90b5e41d3345facf0990f8206d8ec74198
Martin Valgur <martin.valgur@gmail.com>
2025-09-27 10:31:24 +0300

    easyexif: add 1.0+openmvg.20200322

--- recipes/easyexif/all/conanfile.py
+++ recipes/easyexif/all/conanfile.py
@@ -11,58 +11,50 @@ required_conan_version = ">=2.1"
 class EasyExifConan(ConanFile):
     name = "easyexif"
     description = "Tiny ISO-compliant C++ EXIF parsing library, third-party dependency free."
-    topics = ("exif", "image", "multimedia", "format", "graphics")
-    homepage = "https://github.com/mayanklahiri/easyexif"
     license = "BSD-2-Clause"
-
+    homepage = "https://github.com/mayanklahiri/easyexif"
+    topics = ("exif", "image", "multimedia", "format", "graphics")
     settings = "os", "arch", "compiler", "build_type"
     package_type = "library"
     options = {
         "shared": [True, False],
         "fPIC": [True, False]
     }
-    default_options = {"shared": False, "fPIC": True}
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            self.options.rm_safe("fPIC")
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+    implements = ["auto_shared_fpic"]
 
     def export_sources(self):
-        copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=self.export_sources_folder)
-
-    @property
-    def _min_cppstd(self):
-        return 11
+        copy(self, "CMakeLists.txt", self.recipe_folder, os.path.join(self.export_sources_folder, "src"))
+        export_conandata_patches(self)
 
     def validate(self):
-        check_min_cppstd(self, self._min_cppstd)
+        check_min_cppstd(self, 11)
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["EASYEXIF_SRC_DIR"] = self.source_folder.replace("\\", "/")
         tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(build_script_folder="..")
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["easyexif"]
         if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.system_libs.append("m")
+            self.cpp_info.system_libs = ["m"]
