bf800dd98a6c75ea9655f3c534c88075de3d42ea
Martin Valgur <martin.valgur@gmail.com>
2025-09-28 22:54:04 +0300

    intel-tcmlib: add Windows support

--- recipes/intel-tcmlib/all/conanfile.py
+++ recipes/intel-tcmlib/all/conanfile.py
@@ -38,32 +38,50 @@ class IntelTcmlibConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("hwloc/[^2.12]", options={"shared": True})
+        # Can't unvendor hwloc on Windows because the libhwloc-15.dll name does not match hwloc.dll from the recipe
+        if self.settings.os != "Windows":
+            self.requires("hwloc/[^2.12]", options={"shared": True})
 
     def validate(self):
         if self.settings.os not in ["Linux", "FreeBSD", "Windows"]:
             raise ConanInvalidConfiguration(f"{self.settings.os} is not supported")
         if self.settings.arch != "x86_64":
             raise ConanInvalidConfiguration(f"{self.settings.arch} is not supported")
-        if not self.dependencies["hwloc"].options.shared:
-            raise ConanInvalidConfiguration("-o hwloc/*:shared=True is required")
+        if self.settings.os != "Windows":
+            if not self.dependencies["hwloc"].options.shared:
+                raise ConanInvalidConfiguration("-o hwloc/*:shared=True is required")
 
     def _get_pypi_package(self, name):
         get(self, **self._packages[name], destination=self.build_folder, strip_root=True)
 
     def build(self):
         self._get_pypi_package("tcmlib")
-        move_folder_contents(self, os.path.join(self.build_folder, "data"), self.source_folder)
 
     def package(self):
-        copy(self, "*", os.path.join(self.source_folder, "share/doc/tcm/licensing"), os.path.join(self.package_folder, "licenses"))
+        if self.settings.os == "Windows":
+            move_folder_contents(self, os.path.join(self.build_folder, "data", "Library"), self.package_folder)
+            copy(self, "*", os.path.join(self.build_folder, "data"), self.package_folder)
+        else:
+            move_folder_contents(self, os.path.join(self.build_folder, "data"), self.package_folder)
+        copy(self, "*", os.path.join(self.package_folder, "share/doc/tcm/licensing"), os.path.join(self.package_folder, "licenses"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
         if self.settings.os in ["Linux", "FreeBSD"]:
+            rm(self, "libhwloc.*", os.path.join(self.package_folder, "lib"))
             for lib in ["tcm", "tcm_debug"]:
                 libfile = f"lib{lib}.so.{self.version}"
-                copy(self, libfile, os.path.join(self.source_folder, "lib"), os.path.join(self.package_folder, "lib"))
                 major = Version(self.version).major
-                os.symlink(libfile, os.path.join(self.package_folder, "lib", f"lib{lib}.so.{major}"))
-                os.symlink(libfile, os.path.join(self.package_folder, "lib", f"lib{lib}.so"))
+                for target in [
+                    f"lib{lib}.so.{major}",
+                    f"lib{lib}.so",
+                ]:
+                    os.unlink(os.path.join(self.package_folder, "lib", target))
+                    os.symlink(libfile, os.path.join(self.package_folder, "lib", target))
+        else:
+            rm(self, "*hwloc*", os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
         self.cpp_info.includedirs = []
+        if self.settings.os == "Windows":
+            self.cpp_info.libdirs = []
+        else:
+            self.cpp_info.bindirs = []
