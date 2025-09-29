9a521e57b50163e7ee8959dc4944eb74a7d0318c
Martin Valgur <martin.valgur@gmail.com>
2025-09-29 15:27:10 +0300

    gm2calc: drop old versions

--- recipes/gm2calc/all/conanfile.py
+++ recipes/gm2calc/all/conanfile.py
@@ -4,7 +4,6 @@ from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import *
-from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
@@ -15,7 +14,6 @@ class Gm2calcConan(ConanFile):
     license = "GPL-3.0"
     homepage = "https://github.com/GM2Calc/GM2Calc"
     topics = ("high-energy", "physics", "hep", "magnetic moment", "muon", "mssm", "2hdm")
-
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -28,29 +26,18 @@ class Gm2calcConan(ConanFile):
     }
     implements = ["auto_shared_fpic"]
 
-    def export_sources(self):
-        export_conandata_patches(self)
-
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("boost/[1.71.0]")
+        self.requires("boost/[^1.71.0]")
         self.requires("eigen/3.4.0", transitive_headers=True)
 
     def validate(self):
-        check_min_cppstd(self, 11)
+        check_min_cppstd(self, 14)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        apply_conandata_patches(self)
-        # CMake v4 support
-        if Version(self.version) < "2.0":
-            replace_in_file(self, "CMakeLists.txt",
-                            "cmake_minimum_required(VERSION 3.1)",
-                            "cmake_minimum_required(VERSION 3.5)")
-        if Version(self.version) < "2.2.0":
-            replace_in_file(self, "src/CMakeLists.txt", "EIGEN3", "Eigen3")
         # Fix src/slhaea.h:25:10: fatal error: boost/algorithm/string/classification.hpp: No such file or directory
         save(self, "src/CMakeLists.txt", "\ninclude_directories(${Boost_INCLUDE_DIRS})", append=True)
         # Disable examples, test and doc
