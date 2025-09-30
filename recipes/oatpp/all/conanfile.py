f59eb23b70038dd9e9507fc3c83635c3a57de831
Martin Valgur <martin.valgur@gmail.com>
2025-09-30 10:34:59 +0300

    oatpp: drop old versions, use static-library on Windows

--- recipes/oatpp/all/conanfile.py
+++ recipes/oatpp/all/conanfile.py
@@ -1,12 +1,10 @@
 import os
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import *
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
-from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
@@ -14,56 +12,55 @@ required_conan_version = ">=2.1"
 class OatppConan(ConanFile):
     name = "oatpp"
     description = "Modern Web Framework for C++"
-    homepage = "https://github.com/oatpp/oatpp"
     license = "Apache-2.0"
+    homepage = "https://github.com/oatpp/oatpp"
     topics = ("oat++", "web-framework")
-
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "with_test_library": [True, False],
+        "build_test_library": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "with_test_library": False,
+        "build_test_library": False,
     }
     implements = ["auto_shared_fpic"]
 
     @property
     def _version(self):
-        version = str(self.version)
-        if version.endswith(".latest"):
-            version = version[:-len(".latest")]
-        return version
+        return str(self.version).replace(".latest", "")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+        if is_msvc(self):
+            self.package_type = "static-library"
+            del self.options.shared
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
         check_min_cppstd(self, 11)
-        if self.settings.os == "Windows" and self.options.shared:
-            raise ConanInvalidConfiguration("oatpp cannot be built as shared library on Windows")
-
-        if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "5":
-            raise ConanInvalidConfiguration("oatpp requires GCC >=5")
 
     def build_requirements(self):
-        if Version(self._version) >= "1.3.0":
-            self.tool_requires("cmake/[>=3.20 <5]")
+        self.tool_requires("cmake/[>=3.20 <5]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        replace_in_file(self, "CMakeLists.txt",
+                        "cmake_minimum_required(VERSION 3.1 FATAL_ERROR)",
+                        "cmake_minimum_required(VERSION 3.5)")
 
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["OATPP_BUILD_TESTS"] = False
         tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
-        if is_msvc(self) and Version(self.version) >= "1.3.0":
-            tc.variables["OATPP_MSVC_LINK_STATIC_RUNTIME"] = is_msvc_static_runtime(self)
-        tc.variables["OATPP_LINK_TEST_LIBRARY"] = self.options.with_test_library
+        tc.variables["OATPP_MSVC_LINK_STATIC_RUNTIME"] = is_msvc_static_runtime(self)
+        tc.variables["OATPP_LINK_TEST_LIBRARY"] = self.options.build_test_library
         tc.generate()
 
     def build(self):
@@ -72,7 +69,7 @@ class OatppConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
@@ -80,26 +77,26 @@ class OatppConan(ConanFile):
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "oatpp")
 
-        include_dir = os.path.join("include", f"oatpp-{self._version}", "oatpp")
-        lib_dir = os.path.join("lib", f"oatpp-{self._version}")
+        include_dir = f"include/oatpp-{self._version}/oatpp"
+        lib_dir =  f"lib/oatpp-{self._version}"
 
         # oatpp
-        self.cpp_info.components["_oatpp"].set_property("cmake_target_name", "oatpp::oatpp")
-        self.cpp_info.components["_oatpp"].includedirs = [include_dir]
-        self.cpp_info.components["_oatpp"].libdirs = [lib_dir]
-        self.cpp_info.components["_oatpp"].libs = ["oatpp"]
+        self.cpp_info.components["oatpp_"].set_property("cmake_target_name", "oatpp::oatpp")
+        self.cpp_info.components["oatpp_"].includedirs = [include_dir]
+        self.cpp_info.components["oatpp_"].libdirs = [lib_dir]
+        self.cpp_info.components["oatpp_"].libs = ["oatpp"]
         if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.components["_oatpp"].system_libs = ["pthread", "m"]
+            self.cpp_info.components["oatpp_"].system_libs = ["pthread", "m"]
         elif self.settings.os == "Windows":
-            self.cpp_info.components["_oatpp"].system_libs = ["ws2_32", "wsock32"]
+            self.cpp_info.components["oatpp_"].system_libs = ["ws2_32", "wsock32"]
 
         # oatpp-test
-        if self.options.with_test_library:
+        if self.options.build_test_library:
             self.cpp_info.components["oatpp-test"].set_property("cmake_target_name", "oatpp-test::oatpp-test")
             self.cpp_info.components["oatpp-test"].includedirs = [include_dir]
             self.cpp_info.components["oatpp-test"].libdirs = [lib_dir]
             self.cpp_info.components["oatpp-test"].libs = ["oatpp-test"]
-            self.cpp_info.components["oatpp-test"].requires = ["_oatpp"]
+            self.cpp_info.components["oatpp-test"].requires = ["oatpp_"]
 
-        # workaround to have all components in the global target
+        # workaround to having all components in the global target
         self.cpp_info.set_property("cmake_target_name", "oatpp::oatpp-test")
